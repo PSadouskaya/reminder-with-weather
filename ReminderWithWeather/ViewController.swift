8ea0f538a08a0e6f5d62fd44a7578cdff48eb039
Polina.Sadovskaya <Polina.Sadovskaya@fleetcor.com>
2023-03-27 20:11:09 +0200

    Initial Commit

---
+++ ReminderWithWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderWithWeather
+//
+//  Created by Polina Sadouskaya on 27/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
