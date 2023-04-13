6f45957d53b6385e8db2244a173aed354ef5467c
Polina.Sadovskaya <Polina.Sadovskaya@fleetcor.com>
2023-04-13 09:51:46 +0200

    add missing file

---
+++ ReminderWithWeatherUITests/ReminderWithWeatherUITestsLaunchTests.swift
@@ -0,0 +1,22 @@
+//
+//  ReminderWithWeatherUITestsLaunchTests.swift
+//  ReminderWithWeatherUITests
+//
+//  Created by Polina Sadouskaya on 27/03/2023.
+//
+import XCTest
+
+final class ReminderWithWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        
+    }
+}
